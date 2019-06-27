4f0677ee9cc519bbd2cbe1dd99454dc808811b24
unknown <IvyLeano@outlook.com>
2019-06-27 18:00:32 -0400

    final commit

---
+++ JAVA-A5/JAVA-A5/src/JAVA-A5/src/AddressBook.java
@@ -0,0 +1,114 @@
+/**********************************************
+Workshop # 5 - AddressBook.java
+Course: JAC444SAB - Summer
+Last Name: Leano - Hill
+First Name: Ivy
+ID: 120331186
+Section: AB
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Signature: Ivy Leano-Hill
+Date: Friday, June 21, 2019
+**********************************************/
+	
+	import javafx.application.Application;
+	import javafx.scene.control.ComboBox;
+	import javafx.geometry.Insets;
+	import javafx.scene.Scene;
+	import javafx.scene.control.Button;
+	import javafx.scene.control.Label;
+	import javafx.scene.control.TextField;
+    import javafx.scene.layout.GridPane;
+	import javafx.stage.Stage;
+	import javafx.scene.layout.ColumnConstraints;;
+
+	
+	public class AddressBook extends Application {
+	  @Override 
+	  public void start(Stage stage) {
+		  
+		//Creating a grid pane with six columns of equal width (70)  
+	    GridPane pane = new GridPane();
+	    pane.getColumnConstraints().add(new ColumnConstraints(70));
+	    pane.getColumnConstraints().add(new ColumnConstraints(70));
+	    pane.getColumnConstraints().add(new ColumnConstraints(70));
+	    pane.getColumnConstraints().add(new ColumnConstraints(70));
+	    pane.getColumnConstraints().add(new ColumnConstraints(70));
+	    pane.getColumnConstraints().add(new ColumnConstraints(70));
+	    
+	    pane.setPadding(new Insets(12, 13, 14, 15));
+	    pane.setHgap(6);
+	    pane.setVgap(6);
+	    
+	    //First Name node
+	    pane.add(new Label("First Name:"), 0, 0);
+	    pane.add(new TextField(), 1, 0, 5, 1);
+
+	    //Last Name node
+	    pane.add(new Label("Last Name:"), 0, 1);
+	    pane.add(new TextField(), 1, 1, 5, 1);
+	    
+	    //City node
+	    pane.add(new Label("      City: "), 0, 2);
+	    pane.add(new TextField(), 1, 2);
+	    
+	    //Combo drop down menu for provinces
+	    ComboBox<String> myComboBox = new ComboBox<String>();
+	    myComboBox.getItems().addAll(
+	    		"AB","BC","MB","NB","NL",
+	    		"NS","NT", "NU", "ON",
+	    		"PE","QC","SK","YT"
+	    		);
+	    
+	    //Province node
+	    pane.add(new Label("   Province: "), 2, 2);
+	    pane.add(myComboBox, 3, 2);
+	    
+	    //Postal Code node
+	    pane.add(new Label("Postal Code:"), 4, 2);
+	    pane.add(new TextField(), 5, 2);
+	     
+	    //Add Button
+	    Button btAdd = new Button("Add");
+	    pane.add(btAdd, 0, 3);
+	    btAdd.setMaxWidth(Double.MAX_VALUE);
+	   
+	    //First Button
+	    Button btFirst = new Button("First");
+	    pane.add(btFirst, 1, 3);
+	    btFirst.setMaxWidth(Double.MAX_VALUE); //
+	    
+	    //Next Button
+	    Button btNext = new Button("Next");
+	    pane.add(btNext, 2, 3);
+	    btNext.setMaxWidth(Double.MAX_VALUE);
+	    
+	    //Previous Button
+	    Button btPrevious = new Button("Previous");
+	    pane.add(btPrevious, 3, 3);
+	    btPrevious.setMaxWidth(Double.MAX_VALUE);
+	    
+	    //Last Button
+	    Button btLast = new Button("Last");
+	    pane.add(btLast, 4, 3);
+	    btLast.setMaxWidth(Double.MAX_VALUE);
+	    
+	    //Update Button
+	    Button btUpdate = new Button("Update");
+	    pane.add(btUpdate, 5, 3);
+	    btUpdate.setMaxWidth(Double.MAX_VALUE);
+	    
+	    Scene scene = new Scene(pane);
+	   
+	    stage.setTitle("Address Book"); // Set the stage title
+	    stage.setScene(scene); // Place the scene in the stage
+	    stage.show(); // Display the stage
+	   
+	  }
+	  
+	  public static void main(String[] args) {
+	    launch(args);
+	  }
+	} 
+
+
+
---
+++ JAVA-A5/JAVA-A5/src/JAVA-A5/src/CheckerBoard.java
@@ -0,0 +1,68 @@
+/**********************************************
+Workshop # 5 - CheckerBoard.java
+Course: JAC444SAB - Summer
+Last Name: Leano - Hill
+First Name: Ivy
+ID: 120331186
+Section: AB
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Signature: Ivy Leano-Hill
+Date: Friday, June 21, 2019
+**********************************************/
+
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene;
+import javafx.stage.Stage; 
+import javafx.scene.shape.Rectangle;
+
+public class CheckerBoard extends Application { 
+   @Override 
+   public void start(Stage stage) { 
+      //Creating an array of rectangles: 8 by 8
+	   Rectangle rectangles[][] = new Rectangle[8][8];
+	   float x; //will be used to set the x-value
+	   float y = 0.0f; // will be used to set the y-value
+	   
+	   //create rectangles and store to 2D array
+	   for(int i = 0; i < 8; i++) {
+		   x = 0.0f; //before starting each row, set the x value equal to 0.0f
+		   for(int j = 0; j < 8; j++) {
+			   rectangles[i][j] = new Rectangle(); 
+			   rectangles[i][j].setX(x); 
+			   rectangles[i][j].setY(y); 
+			   rectangles[i][j].setWidth(37.0f); // I chose 37.0f and 40.0f since my stage is 296 by 320 and if you divide each by 8 equal rectangles 
+			   rectangles[i][j].setHeight(40.0f); // you get a 37.0f by 40.0f dimension
+			   x = x + 37.0f;	// if each rectangle is 37.0f wide then x will increment at 37.0f until row is complete   
+		   }
+		   y = y + 40.0f; // if each rectangle is 40.0f wide then x will increment at 40.0f 
+	   }		
+	   
+	      //Creating a Group object of alternating rectangles
+	       Group root = new Group(
+	    		   rectangles[0][1], rectangles[0][3], rectangles[0][5], rectangles[0][7],
+	    		   rectangles[1][0], rectangles[1][2], rectangles[1][4], rectangles[1][6],
+	    		   rectangles[2][1], rectangles[2][3], rectangles[2][5], rectangles[2][7],
+	    		   rectangles[3][0], rectangles[3][2], rectangles[3][4], rectangles[3][6],
+	    		   rectangles[4][1], rectangles[4][3], rectangles[4][5], rectangles[4][7],
+	    		   rectangles[5][0], rectangles[5][2], rectangles[5][4], rectangles[5][6],
+	    		   rectangles[6][1], rectangles[6][3], rectangles[6][5], rectangles[6][7],
+	    		   rectangles[7][0], rectangles[7][2], rectangles[7][4], rectangles[7][6]
+	    		  );
+	         
+	      //Creates a scene based on the root, and specifying it to be 296 by 320
+	      Scene scene = new Scene(root, 296, 320);  
+	         
+	      //Adds the scene to the stage
+	      stage.setScene(scene); 
+	         
+	      //Displays all contents and sets the title to "Chess Board"
+	      stage.show(); 
+	      stage.setTitle("Chess Board");
+	     
+   }     
+   //main 
+   public static void main(String args[]){ 
+      launch(args); 
+   } 
+} 
